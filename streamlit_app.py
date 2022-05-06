543c72ad3dbb86ba753860b0f2f01cc74ae89b7c
atiqureee51 <60899605+atiqureee51@users.noreply.github.com>
2022-05-06 11:32:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,6 @@ weather, metadata=pvlib.iotools.get_psm3(lat, lon, NREL_API_KEY, email=email, na
 
 
 
-
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
