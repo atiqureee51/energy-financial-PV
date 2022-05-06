9ef92dc38b098027e54129640844d14a9d68b8b9
atiqureee51 <60899605+atiqureee51@users.noreply.github.com>
2022-05-06 13:58:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,28 +28,11 @@ import numpy as np
 
 st.title('Technical and Financial Model of a PV system')
 
-#DATE_COLUMN = 'date/time'
-#DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
-'''
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 
 
-#data_load_state = st.text('Loading data...')
-#data = load_data(10000)
-#data_load_state.text("Done! (using st.cache)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-'''
 
 
 ## 1. Enter the location of the PV system and obtain a weather file for that location
