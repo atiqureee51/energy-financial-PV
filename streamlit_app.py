8db38b8b180428ba750c4f0288f22684a3ac4f24
atiqureee51 <60899605+atiqureee51@users.noreply.github.com>
2022-05-06 19:00:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,23 @@ from itertools import cycle
 
 st.title('Technical and Financial Model of a PV system')
 
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
 @st.cache(allow_output_mutation=True)
 
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+#data_load_state = st.text('Loading data...')
+data = load_data(10000)
+#data_load_state.text("Done! (using st.cache)")
 
 #if st.checkbox('Show raw data'):
 #    st.subheader('Raw data')
